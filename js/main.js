b225b4d0ad0937c73d87368eb709a1705c18ecae
denisetroglio <overbeck.denise@gmail.com>
2021-12-28 21:23:39 +0100

    add js API and function

--- js/main.js
+++ js/main.js
@@ -1,5 +1,25 @@
 "use strict";
 
+const sectionAnimes = document.querySelector(".js_animes");
+
 fetch("https://api.jikan.moe/v3/search/anime?q=naruto")
-.then(response => response.json())
-.then(data => console.log(data));
+  .then((response) => response.json())
+  .then((data) => console.log(data));
+
+const data = [
+
+];
+
+
+
+
+function renderArticle(event) {
+    event.preventDefault();
+  sectionAnimes.innerHTML += `<article class="article">
+<img src="https://cdn.myanimelist.net/images/anime/5/17407.jpg?s=2bf24a22a339223dcadb1cdfc3307b61" class="card__img" alt="Animes">
+<button class="card__btn" title="Añadir a favoritos">Añadir a favoritos</button></article>`;
+}
+
+
+sectionAnimes.innerHTML = "";
+renderArticle();
