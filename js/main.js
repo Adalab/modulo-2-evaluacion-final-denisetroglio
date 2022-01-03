8549232c3280a7eada914bee3450de3fb7f3edbd
denisetroglio <overbeck.denise@gmail.com>
2022-01-03 22:00:47 +0100

    add new styles and x bottom

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ const userValue = document.querySelector(".js_search");
 const resetBtn = document.querySelector(".js_reset");
 const listFav = document.querySelector(".js_fav");
 const buttonSearch = document.querySelector(".js_button");
+const buttonX = document.querySelector(".js_deletefav");
 
 let animes = [
   {
@@ -31,7 +32,7 @@ const getApiData = () => {
 const getAnimes = (anime) => {
   let animeHtmlCode = "";
   animeHtmlCode += `<li class="list_animes js_listresults">`;
-  animeHtmlCode += `<img src="${anime.image_url}"  data-id="${anime.mal_id}" class="card__img js_addanimefav" alt="animes">`;
+  animeHtmlCode += `<img src="${anime.image_url}"  data-id="${anime.mal_id}" class="card__imgres js_addanimefav" alt="animes">`;
   animeHtmlCode += `<h2 class="title_anime">${anime.title}</h2></li>`;
   return animeHtmlCode;
 };
@@ -76,39 +77,47 @@ const addToFavorites = (ev) => {
   });
   console.log(favorites);
   paintFavorites();
+  setInLocalStorage();
 };
 
 const getToFavorites = (fav) => {
   let animeFavHtmlCode = "";
   animeFavHtmlCode += `<li class="list_animesfav js_fav">`;
-  animeFavHtmlCode += `<img src="${fav.image_url}"  data-id="${fav.mal_id}" class="card__img js_addanimefav" alt="animes">`;
+  animeFavHtmlCode += `<img src="${fav.image_url}"  data-id="${fav.mal_id}" class="card__imgfav js_addanimefav" alt="animes">`;
   animeFavHtmlCode += `<h2 class="title_anime">${fav.title}</h2></li>`;
+  animeFavHtmlCode += `<button class="delete js_deletefav">x</button>`;
   return animeFavHtmlCode;
 };
 
 //pintar en favoritos:
 const paintFavorites = () => {
   listFav.innerHTML = "";
-  for(const fav of favorites){
-  listFav.innerHTML += getToFavorites(fav);
-} 
+  for (const fav of favorites) {
+    listFav.innerHTML += getToFavorites(fav);
+  }
 };
 
+//Boton "x" Favoritos:
 
 
 
 
-//Boton reset (se apaga en favoritos):
+//Boton reset (se apaga los favoritos):
 function handleClickReset() {
-  for (eachClick of resetBtn) {
-    listFav.innerHTML = "";
-  }
-  resetAllResults();
+  listFav.innerHTML = "";
 }
+handleClickReset();
+
+//Local Storage:
+const setInLocalStorage = () => {
+  const storageFav = JSON.stringify(favorites);
+  localStorage.setItem("fav", storageFav);
+};
 
 //Eventos:
-resetBtn.addEventListener("reset", handleClickReset); // Boton reset
+resetBtn.addEventListener("click", handleClickReset); // Boton reset
 buttonSearch.addEventListener("click", handleClickSearch); // Boton de buscar
+/*buttonX.addEventListener("click", handleClickX); //Boton "x" Favoritos*/
 
 //Start app:
 //getApiData();
