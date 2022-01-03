8f645778e888314aaab3542834440501835f8eca
denisetroglio <overbeck.denise@gmail.com>
2022-01-03 13:37:48 +0100

    add JS: Results, Favorites

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const animesElement = document.querySelector(".js_results");
-const searchInput = document.querySelector(".js_search");
+const userValue = document.querySelector(".js_search");
 const resetBtn = document.querySelector(".js_reset");
 const listFav = document.querySelector(".js_fav");
 const buttonSearch = document.querySelector(".js_button");
@@ -18,7 +18,7 @@ let favorites = [];
 
 // Get data from API:
 const getApiData = () => {
-  fetch("https://api.jikan.moe/v3/search/anime?q=")
+  fetch(`https://api.jikan.moe/v3/search/anime?q=${userValue.value}`)
     .then((response) => response.json())
     .then((data) => {
       animes = data.results;
@@ -31,33 +31,23 @@ const getApiData = () => {
 const getAnimes = (anime) => {
   let animeHtmlCode = "";
   animeHtmlCode += `<li class="list_animes js_listresults">`;
-  animeHtmlCode += `<img src="${anime.image_url}"  data-id=${anime.mal_id} class="card__img js_addanimefav" alt="animes">`;
+  animeHtmlCode += `<img src="${anime.image_url}"  data-id="${anime.mal_id}" class="card__img js_addanimefav" alt="animes">`;
   animeHtmlCode += `<h2 class="title_anime">${anime.title}</h2></li>`;
   return animeHtmlCode;
 };
 const paintAnimes = () => {
   let animesCode = "";
-  for (const anime of animesCode) {
+  for (const anime of animes) {
     animesCode += getAnimes(anime);
   }
   animesElement.innerHTML = animesCode;
   listAnimesFav();
 };
 
-//Buscar animes (digitar):
-function handleSearchInput(event) {
-  event.preventDefault;
-  if (searchInput.value.lenght >= 4) {
-  }
-}
-
 //Boton buscar animes (click boton):
-function handleClickSearch() {
-  for (eachSearch of buttonSearch) {
-    animesElement.innerHTML += "";
-    console.log("busco algun anime");
-  }
-  paintResults();
+function handleClickSearch(event) {
+  event.preventDefault();
+  getApiData();
 }
 
 //Añadir imagenes de resultados a favoritos:
@@ -67,25 +57,50 @@ const listAnimesFav = () => {
     addAnimeFav.addEventListener("click", addToFavorites);
   }
 };
+
 const addToFavorites = (ev) => {
-  const clickToFavId = ev.target.mal_id;
+  //console.log(ev.target.dataset.id);
+
+  const clickedId = parseInt(ev.target.dataset.id);
   let foundAnime;
   for (const anime of animes) {
-    if (anime.mal_id === clickToFavId) {
+    if (anime.mal_id === clickedId) {
       foundAnime = anime;
     }
   }
+
   favorites.push({
     mal_id: foundAnime.mal_id,
     image_url: foundAnime.image_url,
     title: foundAnime.title,
   });
+  console.log(favorites);
+  paintFavorites();
 };
 
-//Boton reset (se apaga en favoritos y en resultados):
+const getToFavorites = (fav) => {
+  let animeFavHtmlCode = "";
+  animeFavHtmlCode += `<li class="list_animesfav js_fav">`;
+  animeFavHtmlCode += `<img src="${fav.image_url}"  data-id="${fav.mal_id}" class="card__img js_addanimefav" alt="animes">`;
+  animeFavHtmlCode += `<h2 class="title_anime">${fav.title}</h2></li>`;
+  return animeFavHtmlCode;
+};
+
+//pintar en favoritos:
+const paintFavorites = () => {
+  listFav.innerHTML = "";
+  for(const fav of favorites){
+  listFav.innerHTML += getToFavorites(fav);
+} 
+};
+
+
+
+
+
+//Boton reset (se apaga en favoritos):
 function handleClickReset() {
   for (eachClick of resetBtn) {
-    listAnimeResults.innerHTML = "";
     listFav.innerHTML = "";
   }
   resetAllResults();
@@ -93,8 +108,7 @@ function handleClickReset() {
 
 //Eventos:
 resetBtn.addEventListener("reset", handleClickReset); // Boton reset
-searchInput.addEventListener("keyup", handleSearchInput); //digitar nombre animes
 buttonSearch.addEventListener("click", handleClickSearch); // Boton de buscar
 
 //Start app:
-getApiData();
+//getApiData();
